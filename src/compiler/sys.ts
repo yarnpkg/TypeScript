7e564312cd2cf1eea8b76d23db4bd2226202ccc3
Clement Yan <yanclement94@gmail.com>
2025-12-08 23:57:20 +0800

    fix: Disable PnP in browser environments

--- src/compiler/sys.ts
+++ src/compiler/sys.ts
@@ -46,6 +46,9 @@ import {
     WatchOptions,
     writeFileEnsuringDirectories,
 } from "./_namespaces/ts";
+import {
+    isPnpEnabled
+} from "./pnpapi";
 
 declare function setTimeout(handler: (...args: any[]) => void, timeout: number): any;
 declare function clearTimeout(handle: any): void;
@@ -1747,7 +1750,7 @@ export let sys: System = (() => {
 
         function isFileSystemCaseSensitive(): boolean {
             // The PnP runtime is always case-sensitive
-            if (typeof process.versions.pnp !== `undefined`) {
+            if (isPnpEnabled()) {
                 return true;
             }
             // win32\win64 are case insensitive platforms
--- src/server/project.ts
+++ src/server/project.ts
@@ -1,5 +1,6 @@
 import {
     getPnpApi,
+    isPnpEnabled,
 } from "../compiler/pnpapi";
 import * as ts from "./_namespaces/ts";
 import {
@@ -2812,7 +2813,7 @@ export class ConfiguredProject extends Project {
     }
 
     updateReferences(refs: readonly ProjectReference[] | undefined) {
-        if (typeof process.versions.pnp !== `undefined`) {
+        if (isPnpEnabled()) {
             // With Plug'n'Play, dependencies that list peer dependencies
             // are "virtualized": they are resolved to a unique (virtual)
             // path that the underlying filesystem layer then resolve back
